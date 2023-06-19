5a9afe50a53dc6f0e6b64ea04f895341e79b4ffb
Sabah Raad <raad@aamarpay.net>
2023-06-19 15:37:42 +0600

    Company added

---
+++ app/Http/Controllers/companyController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class companyController extends Controller
+{
+    public function addCompany(Request $request){
+        dd($request);
+    }
+
+    public function showCompany(){
+
+    }
+
+    public function editCompany(){
+
+    }
+
+    public function deleteCompany(){
+        
+    }
+}
