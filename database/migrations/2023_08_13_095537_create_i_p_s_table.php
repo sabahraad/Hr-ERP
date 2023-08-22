731b0ea81c313d4cc081330900748a8df9351b03
Sabah Raad <raad@aamarpay.net>
2023-08-22 10:43:12 +0600

    IP Model added

---
+++ database/migrations/2023_08_13_095537_create_i_p_s_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('i_p_s', function (Blueprint $table) {
+            $table->increments('ip_id');
+            $table->string('ip');
+            $table->string('wifiName')->nullable();
+            $table->string('company_id');
+            $table->foreign('company_id')->references('ip_id')->on('companies')->onDelete('cascade');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('i_p_s');
+    }
+};
