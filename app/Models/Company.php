332f8735f80dd463ae2aad7ef8753c98e0c4798a
Sabah Raad <raad@aamarpay.net>
2023-08-27 17:37:49 +0600

    Role check added

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\ValidationException;
+
 
 class Company extends Model
 {
