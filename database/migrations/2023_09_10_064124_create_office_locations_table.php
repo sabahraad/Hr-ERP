39c44ff629c01d230f9d9f9f2a1a51e48b2abc53
Sabah Raad <raad@aamarpay.net>
2023-09-10 17:49:50 +0600

    office location crud added

---
+++ database/migrations/2023_09_10_064124_create_office_locations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('office_locations', function (Blueprint $table) {
+            $table->id('office_locations_id');
+            $table->string('location_name');
+            $table->double('latitude');
+            $table->double('longitude');
+            $table->double('radius');
+            $table->string('status')->default('1');
+            $table->unsignedBigInteger('company_id');
+            $table->foreign('company_id')->references('company_id')->on('companies')->onDelete('cascade');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('office_locations');
+    }
+};
