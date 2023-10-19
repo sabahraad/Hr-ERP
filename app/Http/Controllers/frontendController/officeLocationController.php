3517a0a6fed7c67d4aad1244691e7b51ac4f9c17
Sabah Raad <raad@aamarpay.net>
2023-10-19 17:50:25 +0600

    location details added

---
+++ app/Http/Controllers/frontendController/officeLocationController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\frontendController;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class officeLocationController extends Controller
+{
+    public function officeLocation(){
+
+        $access_token = session('access_token');
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://hrm.aamarpay.dev/api/office-location-list',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer ' . $access_token),
+        ));
+        $response = curl_exec($curl);
+        curl_close($curl);
+        $dataArray = json_decode($response,true);
+        return view('frontend.officeLocation',compact('dataArray'), ['jwtToken' => $access_token]);
+    }
+}
