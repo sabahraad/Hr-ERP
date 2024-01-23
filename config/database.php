41d2ea611f5e31591cb3f49f02c135cfa603a77e
Sabah Raad <raad@aamarpay.net>
2024-01-23 17:33:47 +0600

    database settings change

--- config/database.php
+++ config/database.php
@@ -43,14 +43,18 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+        # DB_DATABASE=hrmdb
+        # DB_USERNAME=hrmuser
+        # DB_PASSWORD=dFaXC2YSrPTy4F8m
+
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'testhrm'),
+            'username' => env('DB_USERNAME', 'testhrmuser'),
+            'password' => env('DB_PASSWORD', 'BaWe2CAy4fjca66K'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
