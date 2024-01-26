b87c639a0a0206eeb40cdee07998cbb1d4e29b47
Sabah Raad <raad@aamarpay.net>
2024-01-26 09:07:05 +0600

    Salary adjustmet added

--- app/Http/Controllers/SalaryController.php
+++ app/Http/Controllers/SalaryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Payslip;
 use App\Models\PreviousSalaryHistroy;
 use App\Models\Salary;
 use Illuminate\Support\Facades\Validator;
@@ -61,4 +62,68 @@ class SalaryController extends Controller
             'data'=>$privouseSalaryDetails
         ],200);
     }
+
+    public function salaryList(){
+        $company_id = auth()->user()->company_id;
+        $data = Salary::where('company_id',$company_id)->get();
+        if(count($data) == 0){
+            return response()->json([
+                'message'=>'No salary list found',
+                'data'=>$data
+            ],200);
+        }else{
+            return response()->json([
+                'message'=>'Salary list',
+                'data'=>$data
+            ],200);
+        }
+    }
+
+    public function createPayslip(Request $request){
+        $month = $request->month;
+        $year = $request->year;
+        $company_id = auth()->user()->company_id;
+        $salary = Salary::where('company_id',$company_id)->get();
+        foreach($salary as $sal){
+            if (!$this->payslipExists($sal->emp_id, $sal->company_id)) {
+                $data = new Payslip();
+                $data->salary = $sal->salary;
+                $data->month = $request->month;
+                $data->year = $request->year;
+                $data->emp_id = $sal->emp_id;
+                $data->company_id = $sal->company_id;
+                $data->save();
+            }
+        }
+        $payslip = Payslip::where('company_id',$company_id)
+                            ->where('month',$request->month)
+                            ->where('year',$request->year)
+                            ->get();
+
+        return response()->json([
+            'message'=>'Payslip List for'.$request->month,
+            'data'=>$payslip
+        ],201);
+    }
+
+    private function payslipExists($empId, $companyId)
+    {
+        return Payslip::where('emp_id', $empId)
+            ->where('company_id', $companyId)
+            ->where('month', now()->format('m'))
+            ->where('year', now()->format('Y'))
+            ->exists();
+    }
+
+    public function adjustPayslip(Request $request,$id){
+        $data = Payslip::find($id);
+        $data->salary = $request->salary;
+        $data->adjustment_reason = $request->adjustment_reason;
+        $data->save();
+        return response()->json([
+            'message'=>'Payslip adjusted successfully',
+            'data'=>$data
+        ],200);
+    }
+
 }
