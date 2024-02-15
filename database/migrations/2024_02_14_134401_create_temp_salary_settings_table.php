546f87d41c1125cae7c2c34bff7031157fc43276
Sabah Raad <raad@aamarpay.net>
2024-02-15 17:27:59 +0600

    temp salary setting added

---
+++ database/migrations/2024_02_14_134401_create_temp_salary_settings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('temp_salary_settings', function (Blueprint $table) {
+            $table->bigIncrements('temp_salary_settings_id');
+            $table->string('name');
+            $table->decimal('percentage');
+            $table->bigInteger('company_id')->unsigned();
+            $table->foreign('company_id')->references('company_id')->on('companies')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('temp_salary_settings');
+    }
+};
