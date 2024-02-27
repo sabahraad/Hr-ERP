a9814b3f9d3d4a60c3e7aed3fe183315601beb9f
Sabah Raad <raad@aamarpay.net>
2024-02-27 18:03:26 +0600

    timeline setting added

---
+++ app/Http/Controllers/frontendController/timelineController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\frontendController;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class timelineController extends Controller
+{
+    public function timelineSetting(){
+        $access_token = session('access_token');
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://hrm.aamarpay.dev/api/timeline-list',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer ' . $access_token),
+        ));
+        $response = curl_exec($curl);
+        curl_close($curl);
+        $dataArray = json_decode($response,true);
+
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://hrm.aamarpay.dev/api/emp-list',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer ' . $access_token),
+        ));
+        $response = curl_exec($curl);
+        curl_close($curl);
+        $employee = json_decode($response,true);
+        return view('frontend.timelineSetting',compact('dataArray','employee'), ['jwtToken' => $access_token]);
+    }
+}
