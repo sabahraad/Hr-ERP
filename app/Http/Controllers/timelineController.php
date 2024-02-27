5884098d6d8cacff8d6bb1ed2f7cd3cb03e5f6e0
Sabah Raad <raad@aamarpay.net>
2024-02-27 18:01:54 +0600

    bug fixed

--- app/Http/Controllers/timelineController.php
+++ app/Http/Controllers/timelineController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Timeline;
+use App\Models\Employee;
+use App\Models\TimelineSetting;
+use App\Models\TimelineTrack;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx\Rels;
 
 class timelineController extends Controller
 {
@@ -16,7 +17,7 @@ class timelineController extends Controller
     public function addTimeline(Request $request){
         $validator = Validator::make($request->all(), [
             'fetch_time' => 'required|string',
-            'emp_id' => 'string|unique:timeline'
+            'emp_id' => 'required|unique:timeline_settings'
         ]);
         
         if ($validator->fails()) {
@@ -26,7 +27,7 @@ class timelineController extends Controller
         }
 
         $company_id = auth()->user()->company_id;
-        $data = new Timeline();
+        $data = new TimelineSetting();
         $data->fetch_time = $request->fetch_time;
         $data->emp_id = $request->emp_id;
         $data->company_id = $company_id;
@@ -44,7 +45,9 @@ class timelineController extends Controller
 
     public function timelineList(){
         $company_id = auth()->user()->company_id;
-        $data = Timeline::where('company_id',$company_id)->get();
+        $data = TimelineSetting::where('timeline_settings.company_id',$company_id)
+                                ->join('employees','employees.emp_id','=','timeline_settings.emp_id')
+                                ->get();
         if(count($data) == 0){
             return response()->json([
                 'message'=>'No data found',
@@ -59,7 +62,7 @@ class timelineController extends Controller
     }
 
     public function editTimeline(Request $request,$id){
-        $data = Timeline::find($id);
+        $data = TimelineSetting::find($id);
         $data->fetch_time = $request->fetch_time;
         $data->save();
         return response()->json([
@@ -69,9 +72,93 @@ class timelineController extends Controller
     }
 
     public function deleteTimeline($id){
-        Timeline::destroy($id);
+        TimelineSetting::destroy($id);
         return response()->json([
             
         ],204);
+    }
+
+    public function storeTimelineTrack(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'track_date' => 'required|date_format:Y-m-d',
+            'latitude' => 'required|numeric ',
+            'longitude' => 'required|numeric'
+        ]);
+        
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => $validator->errors(),
+            ], 422);
+        }
+
+        $user_id = auth()->user()->id;
+        $emp_id = Employee::where('id',$user_id)->value('emp_id');
+        $company_id = auth()->user()->company_id;
+        $currentTime = date('H:i:s');
+        $existingRecord = TimelineTrack::where('track_date', $request->track_date)
+                               ->where('emp_id', $emp_id)
+                               ->first();
+
+        if ($existingRecord) {
+            $existingLocationData = json_decode($existingRecord->location, true) ?? [];
+            $newLocationData = [
+                'latitude' => $request->latitude,
+                'longitude' => $request->longitude,
+                'time' => $currentTime
+            ];
+            $existingLocationData[] = $newLocationData;
+            $existingRecord->location = json_encode($existingLocationData);
+            $existingRecord->save();
+
+            return response()->json([
+                'message' => 'TimeLine Track Successfully Stored',
+                'data' => $existingRecord
+            ], 201);
+        } else {
+            $data = new TimelineTrack();
+            $data->track_date = $request->track_date;
+            $newLocationData = [
+                'latitude' => $request->latitude,
+                'longitude' => $request->longitude,
+                'time' => $currentTime
+            ];
+            $data->location = json_encode([$newLocationData]);
+            $data->emp_id = $emp_id;
+            $data->company_id = $company_id;
+            $data->save();
+
+            return response()->json([
+                'message' => 'TimeLine Track Successfully Stored',
+                'data' => $data
+            ], 201);
+        }
+    }
+
+    public function dateWiseTrackOfEmployee(Request $request){
+        $validator = Validator::make($request->all(), [
+            'track_date' => 'required|date_format:Y-m-d',
+            'emp_id' => 'required'
+        ]);
+        
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => $validator->errors(),
+            ], 422);
+        }
+        $date = $request->track_date;
+        $emp_id = $request->emp_id;
+        $data = TimelineTrack::where('track_date',$date)->where('emp_id',$emp_id)->first();
+        if(!$data){
+            return response()->json([
+                'message'=>'No Timeline Track found',
+                'data'=>$data
+            ],404);
+        }else{
+            return response()->json([
+                'message'=>'Timeline Track details',
+                'data'=>$data
+            ],200);
+        }
     }
 }
