3346d26abae92d5cf4c5c26c1f51207b3405c077
Sabah Raad <raad@aamarpay.net>
2024-05-09 15:02:35 +0600

    super admin check added

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,6 +66,6 @@ class Kernel extends HttpKernel
         'roleCheck' => \App\Http\Middleware\RoleCheck::class,
         'check_access_token' => \App\Http\Middleware\CheckAccessToken::class,
         'prevent-back-history' => \App\Http\Middleware\PreventBrowserBackHistroy::class,
-
+        'super_admin_check' => \App\Http\Middleware\SuperAdminCheck::class,
     ];
 }
---
+++ app/Http/Middleware/SuperAdminCheck.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SuperAdminCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!session('access_token') && session('role') !== 3) {
+            return redirect('/login')->with('error', 'You do not have permission to log in.');
+        }
+
+        return $next($request);
+        
+    }
+}
+// !session('access_token') &&
