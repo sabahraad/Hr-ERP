3e2ab9ecaaff83771dc6dd86e77f18d05ae8b139
Sabah Raad <raad@aamarpay.net>
2024-05-21 13:58:57 +0600

    admin and finance roll added

---
+++ app/Http/Middleware/adminCheck.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class adminCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (session()->has('access_token') && !empty(session()->get('access_token')) && (session('role') == 4 || session('role') == 5) ) {
+            return $next($request);
+        }
+        else{
+            return redirect('/login')->with('error', 'You do not have permission to log in.');
+        }
+        
+    }
+}
