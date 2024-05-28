f35a3af729c01ebdf14a71351fd8bd79e65cee8e
Sabah Raad <raad@aamarpay.net>
2024-05-28 17:50:07 +0600

    id bug fixed

--- app/Http/Controllers/employeeController.php
+++ app/Http/Controllers/employeeController.php
@@ -317,7 +317,9 @@ class employeeController extends Controller
     }
 
     public function employeeDetails($id){
+        $company_id= auth()->user()->company_id;
         $data = Employee::where('employees.emp_id', $id)
+                        ->where('company_id',$company_id)
                         ->join("users", "users.id", "=", "employees.id")
                         ->leftJoin("salaries", "salaries.emp_id", "=", "employees.emp_id")
                         ->get(['employees.*', 'users.email', 'salaries.salary']);
