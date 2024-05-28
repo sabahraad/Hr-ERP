c6f73157b0a7a701207078e1f5072a4ac73934a7
Sabah Raad <raad@aamarpay.net>
2024-05-28 15:52:21 +0600

    bug solved

--- app/Http/Controllers/SalaryController.php
+++ app/Http/Controllers/SalaryController.php
@@ -114,7 +114,6 @@ class SalaryController extends Controller
         $year = $request->year;
         $company_id = auth()->user()->company_id;
         $salary = Salary::where('company_id',$company_id)->get();
-        dd($salary);
         if(count($salary) == 0){
             return response()->json([
                 'message'=>'Please set salary for employee first'
