9ae452f8e5dc1df3efc1e05a4603ae751e7f72e1
Sabah Raad <raad@aamarpay.net>
2024-08-01 16:04:55 +0600

    attendance stellar log added

--- config/logging.php
+++ config/logging.php
@@ -126,6 +126,12 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+
+        'attendance' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/attendance.log'),
+            'level' => 'error',
+        ],
     ],
 
 ];
